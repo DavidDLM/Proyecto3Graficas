084a67e606d3685570b1d4027cc3637c8d7566ae
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-11-30 23:53:58 -0600

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -13,22 +13,22 @@ class Renderer:
     def __init__(this, game):
         this.game = game
         this.SCREEN = game.SCREEN
-        this.wallTXT = this.loadWallTXT()
-        this.sky = this.getTXT('models/background2.png', (WIDTH, HEIGH_HF))
+        this.wallTXT = this.loadWallTXT() # Cargar texturas
+        this.sky = this.getTXT('models/background2.png', (WIDTH, HEIGH_HF)) # Cargar fondo
         this.skyOffset = 0
 
-    def draw(this):
+    def draw(this): # Dibujar el fondo y los objetos
         this.drawBckg()
         this.renderObj()
 
-    def drawBckg(this):
+    def drawBckg(this): # Fondo
         this.skyOffset = (this.skyOffset + 4.5 * this.game.player.rel) % WIDTH
         this.SCREEN.blit(this.sky, (-this.skyOffset, 0))
         this.SCREEN.blit(this.sky, (-this.skyOffset + WIDTH, 0))
         # Floor color
         pygame.draw.rect(this.SCREEN, FLOOR_CLR, (0, HEIGH_HF, WIDTH, HEIGHT))
 
-    def renderObj(this):
+    def renderObj(this): # Objetos
         objects = sorted(this.game.raycasting.objects,
                          key=lambda t: t[0], reverse=True)
         for depth, image, pos in objects:
@@ -39,7 +39,7 @@ class Renderer:
         txt = pygame.image.load(path).convert_alpha()
         return pygame.transform.scale(txt, res)
 
-    def loadWallTXT(this):
+    def loadWallTXT(this): # Cargar texturas de paredes
         return {
             1: this.getTXT('models/metalwall2.png'),
             2: this.getTXT('models/metalwall.png'),
