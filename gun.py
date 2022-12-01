6da85f92c917edefd735bf42731dcb050dd136a2
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-11-30 23:47:57 -0600

    Sin terminar :(

--- gun.py
+++ gun.py
@@ -9,13 +9,13 @@ from sprites import *
 
 
 class Gun(AnimatedSprite):
-    def __init__(this, game, path='models/sprites/animated/gun/1.png', scale=3, time=90):
+    def __init__(this, game, path='models/sprites/animated/gun/1.png', scale=3, time=90): # Hereda de AnimatedSprite
         super().__init__(game=game, path=path, scale=scale, time=time)
         this.imgs = deque(
             [pygame.transform.smoothscale(img, (this.image.get_width() * scale, this.image.get_height() * scale))
              for img in this.imgs])
         this.gunPos = (
-            WIDTH_HF + (this.imgs[0].get_width() * 0.5), HEIGHT - this.imgs[0].get_height())
+            WIDTH_HF + (this.imgs[0].get_width() * 0.5), HEIGHT - this.imgs[0].get_height()) # Posicion del arma
         this.numImgs = len(this.imgs)
         this.reload = False
         this.frames = 0
@@ -24,7 +24,7 @@ class Gun(AnimatedSprite):
         if this.reload:
             this.game.player.fire = False
             if this.trigger:
-                this.imgs.rotate(-1)
+                this.imgs.rotate(-1) # Se cambia de animaciones
                 this.image = this.imgs[0]
                 this.frames += 1
                 if this.frames == this.numImgs:
@@ -32,7 +32,7 @@ class Gun(AnimatedSprite):
                     this.frames = 0
 
     def drawGun(this):
-        this.game.SCREEN.blit(this.imgs[0], this.gunPos)
+        this.game.SCREEN.blit(this.imgs[0], this.gunPos) # Dibuja el arma
 
     def update(this):
         this.compareTime()
